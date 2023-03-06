fc8d039bb410b58e1d4057f72932c3027647adb2
ReallyNotBob <97850209+ReallyNotBob@users.noreply.github.com>
2023-03-06 14:54:12 -0600

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "Arkansas Regional",
+      "defaultValue": "Shawnee Regional",
       "required": "true",
       "disabled": "true"
     },
